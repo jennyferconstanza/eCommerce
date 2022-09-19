9865fe781544fbdb114bd1d9ac33cf09172182c7
Jennyfer Constanza <jennyferislas@gmail.com>
2022-09-18 18:41:45 -0700

    update models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes. INTEGER,
+      notNull: true, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes. STRING, 
+      notNull: true
+    }
   },
   {
     sequelize,
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,7 +1,9 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Category = require("./Category");
+
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +12,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      notNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      notNull: true,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      notNull: true,
+      isDecimal: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      notNull: true,
+      defaultValue: 10
+  },
+
+ category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
